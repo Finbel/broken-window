1273e2fe1ab9d8637b4a99c5896cc2c490397f1a
Patrick O'Regan <patrick.oregan@pricerunner.com>
2022-06-21 19:56:38 +0200

    removed dead code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState("100vw");
+  const [windowScale, setWindowScale] = useState(1);
+  const using = "innerWidth";
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setWindowScale(window.visualViewport.scale);
+      setWindowWidth(`${width}px`);
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        minWidth: windowWidth,
+        minHeight: "100vh",
+        backgroundColor: "teal",
+        fontSize: "1.4em",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#1976d2",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          color: "white",
+          minWidth: windowWidth,
+          minHeight: "64px",
+        }}
+      >
+        header inside the container with dynamically set width
+        <div
+          style={{
+            minWidth: 400,
+            color: "black",
+            backgroundColor: "cyan",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          my min-width is 400px
+        </div>
+      </div>
+      <div
+        style={{
+          color: "cyan",
+          padding: 16,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        the container wrapping everything has a dynamic width
+        <div
+          style={{
+            color: "black",
+            width: "100%",
+            maxWidth: 700,
+            height: 300,
+            backgroundColor: "white",
+            textAlign: "center",
+          }}
+        >
+          my width is 100% and my max-width is 700px
+        </div>
+        <p style={{ margin: 6 }}>Using {using}</p>
+        <p style={{ margin: 6 }}>Width</p>
+        <p style={{ margin: 6 }}>{windowWidth}</p>
+        <p style={{ margin: 6 }}>Viewport scale</p>
+        <p style={{ margin: 6 }}>{windowScale}</p>
+      </div>
     </div>
   );
 }
